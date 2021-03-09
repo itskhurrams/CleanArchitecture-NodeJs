d9e44c5eded1e6d97511b1ca5c9f1d2b3d2caf5a
itskhurram <itskhurrams@outlook.com>
2021-03-10 00:56:45 +0500

    mongo initilization is done

--- persistance/orm/mongoose/mongooseConfiguration.js
+++ persistance/orm/mongoose/mongooseConfiguration.js
@@ -9,7 +9,7 @@ mongoose.connect(environment.database.url, {
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+//db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('connected to MongoDB database!');
 });
